57887e4ee48908ed1b8e37c6345eea17aca96f9f
Alina Salinas <142938656+Serenity-24@users.noreply.github.com>
2024-05-20 10:17:12 -0700

    Update breakout.js

--- breakout.js
+++ breakout.js
@@ -1,66 +1,232 @@
-*{
-    box-sizing: border-box;
+rulesBtn = document.getElementById('rules-btn')
+rules = document.getElementById('rules')
+closeBtn = document.getElementById('close-btn')
+canvas = document.getElementById('canvas')
+ctx = canvas.getContext('2d')
+
+score = 0
+
+brickRowCount = 9
+brickColumnCount = 5
+
+
+//Create ball properties
+ball = {
+    x:canvas.width / 2,
+    y: canvas.height /2,
+    size:10,
+    speed:4,
+    dx: 4,
+    dy: -4,
 }
 
-body{
-    background-color: #0095dd;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    min-height: 100vh;
-    margin:0;
-    font-family: Ariel, Helvetica, sans-serif;
+//create paddle properties
+paddle = {
+    x: canvas.width / 2-40,
+    y: canvas.height - 20,
+    w: 80,
+    h: 10,
+    speed: 8,
+    dx:0,
 }
 
-canvas{
-    background: #f0f0f0;
-    display: block;
-    border-radius: 5px;
+
+
+//Create brick properties
+brickInfo = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX:45,
+    offsetY:60,
+    visible: true,
 }
 
-h1{
-    font-size: 45px;
-    color: #ffffff;
+//Create bricks
+bricks = []
+for(let i = 0; i < brickRowCount; i++) {
+    bricks[i]=[]
+    for(let j = 0; j < brickColumnCount; j++) {
+        const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX
+        const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY
+        bricks[i][j]={x, y, ...brickInfo}
+    }
 }
 
-.btn{
-    cursor:pointer;
-    border: 0;
-    padding: 10px 20px;
-    background: #000000;
-    color: #ffffff;
-    border-radius: 5px;
+
+// Draw ball on canvas
+function drawBall(){
+    ctx.beginPath()
+    ctx.arc(ball.x,ball.y,ball.size,0, Math.PI * 2, true)
+    ctx.fillStyle = '#0095dd'
+    ctx.fill()
+    ctx.closePath()
 }
 
-.brn:hover{
-    background: #222222;
+// Draw paddle on canvas
+function drawPaddle(){
+    ctx.beginPath()
+    ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h)
+    ctx.fillStyle = '#0095dd'
+    ctx.fill()
+    ctx.closePath()
 }
 
-.btn:active{
-    transform: scale(0.98);
+
+
+//Draw score on canvas
+function drawScore(){
+    ctx.font = '20px Ariel'
+    ctx.fillText(`Score:${score}`, canvas.width-100, 30)
+}
+//Draw bricks on canvas
+function drawBricks(){
+    bricks.forEach(column => {
+        column.forEach(brick => {
+            ctx.beginPath()
+            ctx.rect(brick.x, brick.y, brick.w, brick.h)
+            ctx.fillStyle = brick.visible ? '#0095dd':'transparent';
+            ctx.fillStyle = '#0095dd'
+            ctx.fill()
+            ctx.closePath()
+        })
+    })
 }
 
-.rules-btn{
-    position: absolute;
-    top: 30px;
-    left: 30px;
+//Draw everything
+function draw(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    drawPaddle()
+    drawBall()
+    drawScore()
+    drawBricks()
 }
 
-.rules{
-    position: absolute;
-    top: 0;
-    left: 0;
-    background: #333333;
-    color: #ffffff;
-    min-height: 100vh;
-    width: 400px;
-    padding: 20px;
-    line-height: 1.5;
-    transform: translateX(-500px);
-    transition: transform 1s ease-in-out;
+
+
+//Move paddle on canvas
+function movePaddle() {
+    paddle.x = paddle.x + paddle.dx
+
+    //wall detection
+    if (paddle.x < 0) {
+        paddle.x = 0
+    }
+    if (paddle.x + paddle.w > canvas.width) {
+        paddle.x = canvas.width - paddle.w
+    }
 }
 
-.rule.show{
-    transform: translateX(0);
+//keydown event
+function keyDown(e) {
+    //console.log(e.key)
+    if (e.key == 'ArrowRight' || e.key == 'Right') {
+        paddle.dx = paddle.speed
+    }
+    if (e.key == 'ArrowLeft' || e.key == 'Left') {
+        paddle.dx = -paddle.speed
+    }
 }
+
+//keyup event
+function keyUp(e){
+    if(e.key == 'ArrowRight' || e.key == 'Right'    || e.key == 'ArrowLeft' || e.key == 'Left') {
+        paddle.dx = 0
+    }
+}
+//Keyboard event handlers
+document.addEventListener('keydown', keyDown)
+document.addEventListener('keyup', keyUp)
+
+function moveBall() {
+    ball.x = ball.x + ball.dx
+    ball.y = ball.y + ball.dy
+
+    //wall collision (top)
+    if (ball.y + ball.size < 0) {
+        ball.dy = -1 * ball.dy
+    }
+
+    //wall collision (right)
+    if (ball.x + ball.size > canvas.width) {
+        ball.dx = -1 * ball.dx
+    }
+
+    //wall collision (bottom)
+    if (ball.y + ball.size > canvas. height) {
+        ball.dy = -1 * ball.dy
+    }
+
+    //wall collision (left)
+    if (ball.x + ball.size < 0) {
+        ball.dx = -1 * ball.dx
+    }
+
+    //paddle collision
+    if (
+        ball.x - ball.size > paddle.x &&
+        ball.x + ball.size < paddle.x + paddle.w &&
+        ball.y + ball.size > paddle.y
+    ) {
+        ball.dy = -1 * ball.speed
+    }
+
+
+    //increase score
+function increaseScore() {
+    score++ //score = score + 1
+
+    if (score == brickRowCount * brickColumnCount) {
+        score = 0
+        showAllBricks()
+    }
+}
+
+    //Brick collision
+    bricks.forEach(column => {
+        column.forEach(brick => {
+        if (brick.visible) {
+            if (
+                ball.x - ball.size > brick.x && // left brick side
+                ball.x + ball.size < brick.x + brick.w && //right
+                ball.y + ball.size > brick.y &&
+                ball.y - ball.size < brick.y + brick.h //bottom
+            ) {
+            ball.dy = -1 * ball.dy
+            brick.visible = false
+            increaseScore()
+            }
+         }
+       }
+    )}
+ )}
+
+ //lose if hit bottom wall
+ if (ball.y + ball.size > canvas)
+
+
+
+function showAllBricks() {
+    brick.forEach(column => {
+        column.forEach(brick => {
+            brick.visible = true
+        })
+    })
+}
+//update canvas drawing and animation
+function update() {
+    moveBall()
+    movePaddle()
+    draw()
+    requestAnimationFrame(update)
+}
+
+update()
+
+//Rules open and close event handlers
+rulesBtn.addEventListener('click',()=>{
+    rules.classList.add('show')
+})
+closeBtn.addEventListener('click',()=>{
+    rules.classList.remove('show')
+})
